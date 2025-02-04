8121bad6c280786223c26c2aaf76a29957c86c1f
shaihansm <smsyedshaihan@gmail.com>
2025-02-05 00:58:21 +0100

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shaihan/sap_planner/src
+# Build directory: /home/shaihan/sap_planner/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("campus_planner")
